a8e6e2e2d0b4d37ba11af4c3190c29609dc39926
h-yusuf <hafiy288@gmail.com>
2023-09-25 10:26:34 +0700

    add file

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ function searchWeather() {
       let humidityElements = document.querySelectorAll(".humidity");
       let speedElements = document.querySelectorAll(".speed");
       let cloudElements = document.querySelectorAll(".cloud");
+      // let formatted  = formattedTime = document.querySelectorAll(".sunrise");
 
       // Iterasi melalui NodeList dan perbarui elemen-elemen dengan data cuaca
       tempElements.forEach((element) => {
@@ -82,6 +83,28 @@ function searchWeather() {
       cloudElements.forEach((element) => {
         element.innerHTML = `${response.clouds.all}%`;
       });
+    //   unix_timestamp.forEach((element) => {
+    //     element.innerHTML = `${response.sys.sunrise}`;
+    //   });
+  // Create a new JavaScript Date object based on the timestamp
+
+  unix_timestamp = `${response.sys.sunrise}`;
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  var date = new Date(unix_timestamp * 1000);
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
+
+  // Will display time in 10:30:23 format
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  // console.log(cityElements);
+
+
+  console.log(formattedTime);
+
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
@@ -130,6 +153,8 @@ function updateLocalTime() {
   
   // Update the time every second (1000 milliseconds)
   setInterval(updateLocalTime, 1000);
+
+
 // Apex Charts
 
 window.addEventListener("load", function () {
